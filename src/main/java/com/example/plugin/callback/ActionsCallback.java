05bd7e652b03565ad058c3a734310428ec8cb367
DESKTOP-P6IQSSF\kid <86883772@qq.com>
2026-08-19 15:00:55 +0800

    Remove legacy com.example.plugin sources

--- src/main/java/com/example/plugin/callback/ActionsCallback.java
+++
@@ -1,8 +0,0 @@
-package com.example.plugin.callback;
-
-import com.fasterxml.jackson.databind.JsonNode;
-
-public interface ActionsCallback {
-  void onSuccess(JsonNode ackPayload);
-  void onError(Throwable t);
-}
--- src/main/java/com/example/plugin/callback/ActionsUpdateCallback.java
+++
@@ -1,7 +0,0 @@
-package com.example.plugin.callback;
-
-import com.fasterxml.jackson.databind.JsonNode;
-
-public interface ActionsUpdateCallback {
-  void onUpdate(JsonNode updatePayload);
-}
