6a9dcc1fe7cfd24e6d81ab232c0d36b27bc8dcfa
DESKTOP-P6IQSSF\kid <86883772@qq.com>
2026-08-24 18:14:02 +0800

    no message

--- src/main/java/com/ultrabar/server/PluginServerListener.java
+++ src/main/java/com/ultrabar/server/PluginServerListener.java
@@ -3,11 +3,20 @@ package com.ultrabar.server;
 import com.ultrabar.plugin.model.TaskUpdatePayload;
 
 public interface PluginServerListener {
-    default void onRegistered(PluginSession session) {}
+    default void onRegistered(PluginSession session) {
+    }
 
-    default void onUnregistered(PluginSession session) {}
+    default void onUnregistered(PluginSession session) {
+    }
 
-    default void onActionsUpdated(PluginSession session) {}
+    default void onActionsUpdated(PluginSession session) {
+    }
 
-    default void onTaskUpdate(PluginSession session, TaskUpdatePayload payload) {}
+    default void onTaskUpdate(PluginSession session, TaskUpdatePayload payload) {
+    }
+
+
+    default void onEvent(PluginSession session) {
+
+    }
 }
